6cc16bfb3f0c4136d5d399f97b4d56461d542fae
formersbach <fabiormersbach@gmail.com>
2026-02-05 19:50:04 +0100

    Add utils and util tests

---
+++ src/smoldynutils/utils.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def gauss_probability_density(x: float, mu: float, sigma: float) -> float:
+    if sigma <= 0:
+        raise ValueError("sigma must be > 0")
+    
+    return (1/(np.sqrt(2*np.pi)*sigma)) * np.exp(-np.square(x-mu)/(2*sigma**2))
+
+def theoretical_brownian_motion_pdf(x, D, t):
+    if D <= 0:
+        raise ValueError("D must be > 0")
+    if t < 0:
+        raise ValueError("t must be >= 0")
+    sigma = np.sqrt(2 * D * t)
+    mu = 0
+    return gauss_probability_density(x, mu, sigma)
+
+def theoretical_MSD(t, D):
+    return 4*D*t
+
+def theoretical_MSD_residue(t, D, epsilon):
+    return 4*D*t + epsilon
---
+++ tests/test_utils.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pytest
+from smoldynutils.utils import *
+
+def test_gaussian_probability():
+    mu = 0
+    sigma = 1
+    expected = 1/np.sqrt(2*np.pi)
+    assert np.isclose(gauss_probability_density(0, mu, sigma), expected)
+    
+    assert np.isclose(gauss_probability_density(-1, mu, sigma), gauss_probability_density(1, mu, sigma))
+
+
+@pytest.mark.parametrize("sigma", [0, -1])
+def test_gaussian_raises_error(sigma):
+    with pytest.raises(ValueError):
+        gauss_probability_density(0, 0, sigma=sigma)
+
+def test_theoretical_brownian_motion():
+    D = 2
+    t = 3
+    x = 0.5
+
+    sigma = np.sqrt(2 * D * t)
+
+    assert np.isclose(theoretical_brownian_motion_pdf(x, D, t), gauss_probability_density(x, 0, sigma))
+
+@pytest.mark.parametrize("D, t", [
+                         (-1, 1),
+                         (1, -1),
+                         (1, 0)
+])
+def test_brownian_error(D, t):
+    with pytest.raises(ValueError):
+        theoretical_brownian_motion_pdf(0, D, t) 
+
+def test_theoretical_msds():
+    assert theoretical_MSD(0, 0) == 0
+    assert theoretical_MSD(1, 1) == 4
+
+    assert theoretical_MSD_residue(0, 0, 0) == 0
+    assert theoretical_MSD_residue(1, 1, 1) == 5
