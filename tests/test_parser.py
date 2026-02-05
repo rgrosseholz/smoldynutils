67af79f970efce43c20b5cb101a4303ab765af9f
formersbach <fabiormersbach@gmail.com>
2026-02-05 18:28:11 +0100

    Numpy fixed size parser tests

---
+++ tests/test_parser.py
@@ -0,0 +1,55 @@
+import pytest
+import numpy as np
+from smoldynutils.parsing import SmoldynParser
+
+sample_smoldyn_file = (
+    "1,1,0,12.7828,2.06726,100\n"
+    "1,1,0,1.01686,8.12141,99\n"
+    "2,1,0,12.8000,2.10000,100\n"
+    "2,1,0,1.02000,8.13000,99\n"    
+)
+
+sample_smoldyn_file_ragged = (
+    "1,1,0,12.7828,2.06726,100\n"
+    "1,1,0,1.01686,8.12141,99\n"
+    "2,1,0,12.8000,2.10000,100\n"
+    "2,1,0,1.02000,8.13000,99\n" 
+    "1,1,0,12.7828,2.06726,100\n"   
+)
+
+empty_file = ("")
+
+def _write_sample(tmp_path):
+    file = tmp_path / "sample.csv"
+    file.write_text(sample_smoldyn_file)
+    return file    
+
+def _write_sample_ragged(tmp_path):
+    file = tmp_path / "sample2.csv"
+    file.write_text(sample_smoldyn_file_ragged)
+    return file    
+
+def _write_empty(tmp_path):
+    file = tmp_path / "sample2.csv"
+    file.write_text(empty_file)
+    return file    
+
+
+def test_fixed_grid_parser(tmp_path):
+    path = _write_sample(tmp_path)
+    parser = SmoldynParser()
+    ts = parser.parse_fixed_grid(str(path))
+    assert len(ts) == 2
+    assert ts[0]
+
+
+    path = _write_sample_ragged(tmp_path)
+    parser = SmoldynParser()
+    with pytest.raises(NotImplementedError):
+        ts = parser.parse_fixed_grid(str(path))
+    
+    path = _write_empty(tmp_path)
+    parser = SmoldynParser()
+    with pytest.raises(ValueError):
+        ts = parser.parse_fixed_grid(str(path))
+    
