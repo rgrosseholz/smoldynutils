dad0b731eb6d2d62658e43f2cb5ffd5a1caf21fe
formersbach <fabiormersbach@gmail.com>
2026-02-18 18:34:14 +0100

    Added msd plot, msd_comparison plot, and diffconst estimation plot.

--- src/smoldynutils/plots.py
+++ src/smoldynutils/plots.py
@@ -54,7 +54,11 @@ def plot_trajectorie(traj: Trajectory, ax: Axes, title: str = "Title") -> Axes:
     """
     if ax is None:
         raise ValueError("No axis to plot onto provided.")
-    ax.plot(traj.x, traj.y)
+    ax.plot(traj.x, traj.y, color="black")
+    ax.scatter(traj.x, traj.y, c=traj.t)
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_title(title)
     return ax
 
 
@@ -76,4 +80,60 @@ def plot_trajectories(trajs: TrajectorySet, ax: Axes, title: str = "Title") -> A
         raise ValueError("No axis to plot onto provided.")
     for traj in trajs:
         plot_trajectorie(traj, ax, title)
+    return ax
+
+
+def plot_msd(msd: Sequence[float], ax: Axes, title: str = "Title") -> Axes:
+    ax.plot(msd, color="blue")
+    ax.set_xlabel("time")
+    ax.set_ylabel("msd")
+    ax.set_title(title)
+    return ax
+
+
+def plot_msd_comparison(
+    msd: np.ndarray, theoretical_msd: np.ndarray, ax: Axes, title: str = "Title"
+) -> Axes:
+    """Lineplot showing the calculated MSD values vs the theoretical expectation.
+
+    Args:
+        msd (np.ndarray): Calculated MSD values
+        theoretical_msd (np.ndarray): Theoretically expected MSD values
+        ax (Axes): Axis onto which will be plotted
+        title (str, optional): Figure title. Defaults to "Title".
+
+    Raises:
+        ValueError: No axis to plot onto provided.
+
+    Returns:
+        Axes: Axis that contains the msd comparison.
+    """
+    if ax is None:
+        raise ValueError("No axis to plot onto provided.")
+    ax = plot_msd(msd, ax)
+    ax.plot(theoretical_msd, color="red")
+    ax.set_xlabel("time")
+    ax.set_ylabel("msd")
+    ax.set_title(title)
+    return ax
+
+
+def plot_diffconst_estimation(
+    diffcoffs: np.ndarray, reference_diffcoff: float, ax: Axes, title: str = "Title"
+) -> Axes:
+    if ax is None:
+        raise ValueError("No axis to plot onto provided.")
+    lower_bound = min(diffcoffs)
+    upper_bound = max(diffcoffs)
+
+    ax.hist(diffcoffs, bins=np.geomspace(lower_bound, upper_bound, 20))
+    ax.set_xscale("log")
+
+    ax.axvline(float(np.mean(diffcoffs)))
+
+    ax.axvline(reference_diffcoff)
+
+    ax.set_xlabel("Diffusion coefficient")
+    ax.set_ylabel("Count")
+    ax.set_title(title)
     return ax
